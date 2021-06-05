f22ed9cb54c347f6144ac114cbf9b794d033aa79
tanshevel <tmshevel@edu.hse.ru>
2021-06-05 21:27:52 +0300

    filtr

--- src/cut.R
+++ src/cut.R
@@ -1,6 +1,6 @@
 source('/Users/segodnyavottak/Documents/GitHub/hse21_H3K27me3_G4_human./src/libr.R')
 
-#NAME <- 'H3K27me3_H9.ENCFF680AKW.hg19'
+NAME <- 'H3K27me3_H9.ENCFF680AKW.hg19'
 #NAME <- 'H3K27me3_H9.ENCFF851PCG.hg19'
 
 
@@ -8,45 +8,35 @@ bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALS
 colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
 bed_df$len <- bed_df$end - bed_df$start
 
-ggplot(bed_df) +
-  aes(x = len) +
-  geom_histogram() +
-  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
-  theme_bw()
-ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
-
-
 
 
 
 library(tidyr)   # replace_na
 library(tibble)  # column_to_rownames
 
-bed_df %>%
-  arrange(-len) %>%
-  nrow()
-
-bed_df <- read.delim(paste0('/Users/segodnyavottak/Desktop/proekt/', NAME, '.bed'), as.is = TRUE, header = FALSE)
-colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
-bed_df$len <- bed_df$end - bed_df$start
-head(bed_df)
+ggplot(new) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(new))) +
+  theme_bw()
+ggsave(paste0('len_hist', NAME, '.init.pdf'), path = OUT_DIR)
 
 new <- bed_df %>%
   arrange(-len) %>%
-  filter(len < 20000) 
+  filter(len < 5000) 
 
 ggplot(new) +
   aes(x = len) +
   geom_histogram() +
   ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(new))) +
   theme_bw()
-ggsave(paste0('len_hist.', NAME, '.filtered.pdf'), path = OUT_DIR)
+ggsave(paste0('len_hist', NAME, '.filtered.pdf'), path = OUT_DIR)
 
 
 
 new %>%
   select(-len) %>%
-  write.table(file='/Users/segodnyavottak/Desktop/proekt/H3K4me3_A549.ENCFF680AKW.hg19.filtered.bed',
+  write.table(file=paste0(DATA_DIR, NAME, 'filtered.bed'),
               col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
 
 
